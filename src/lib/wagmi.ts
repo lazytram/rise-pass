2e0fad442fafd6b2fa794f10c3fdf57fb05d64b3
LazyTram <nitramcay@gmail.com>
2025-08-21 15:01:16 +0200

    fix: Wallet connect

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,19 +1,12 @@
-import { createConfig, http } from "wagmi";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { riseTestnet } from "wagmi/chains";
-import { injected, walletConnect } from "wagmi/connectors";
 
-export const config = createConfig({
+export const config = getDefaultConfig({
+  appName: "RISE Passport",
+  projectId:
+    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "rise-passport-id",
   chains: [riseTestnet],
-  connectors: [
-    injected(),
-    walletConnect({
-      projectId:
-        process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "your-project-id",
-    }),
-  ],
-  transports: {
-    [riseTestnet.id]: http(riseTestnet.rpcUrls.default.http[0]),
-  },
+  ssr: true,
 });
 
 export const chains = [riseTestnet];
